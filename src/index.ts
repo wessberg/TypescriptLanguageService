bf06b0d3ddbc48f7929c4b3a500851ca34447cce
wessberg <frederikwessberg@gmail.com>
2017-09-10 00:53:12 +0200

    Added interface as export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export {ITypescriptLanguageService} from "./typescript-language-service/i-typesc
 export {TypescriptLanguageService} from "./typescript-language-service/typescript-language-service";
 export {ITypescriptLanguageServiceAddFileOptions} from "./typescript-language-service/i-typescript-language-service-add-file-options";
 export {ITypescriptLanguageServiceGetFileOptions} from "./typescript-language-service/i-typescript-language-service-get-file-options";
+export {ITypescriptLanguageServiceImportPath} from "./typescript-language-service/i-typescript-language-service-import-path";
